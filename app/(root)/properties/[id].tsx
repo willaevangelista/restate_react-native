3a0b334687fef8cebc95e6eb74caa5a69118bc89
Willa Evangelista <willaevangelista.pro@gmail.com>
2025-05-05 14:03:11 -0300

    Setup Routes

---
+++ app/(root)/properties/[id].tsx
@@ -0,0 +1,16 @@
+import { useLocalSearchParams } from 'expo-router';
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Property = () => {
+
+    const { id } = useLocalSearchParams();
+
+    return (
+        <View>
+            <Text>Property</Text>
+        </View>
+    )
+}
+
+export default Property;
